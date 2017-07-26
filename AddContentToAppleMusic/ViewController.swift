95a8090ab63fbb05ee0e4fd801959532a225e944
Marvin Lin <atimis19@gmail.com>
2017-07-26 14:52:09 +0800

    Starting to analyze sample code from Apple

---
+++ AddContentToAppleMusic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AddContentToAppleMusic
+//
+//  Created by moon on 2017/7/26.
+//  Copyright © 2017年 Marvin Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
