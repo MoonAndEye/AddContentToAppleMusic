e5bddcd4958aa0e1a72a810d2d25c82b4f28767e
Marvin Lin <atimis19@gmail.com>
2017-07-26 16:36:01 +0800

    try this

---
+++ AddContentToAppleMusic/tkinfo.swift
@@ -0,0 +1,12 @@
+//
+//  tkinfo.swift
+//  AddContentToAppleMusic
+//
+//  Created by moon on 2017/7/26.
+//  Copyright © 2017年 Marvin Lin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+var test = "testit"
