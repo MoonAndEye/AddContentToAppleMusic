56ac46388d550e991d2f66ece4c681565a42cf31
Marvin Lin <atimis19@gmail.com>
2017-07-26 18:13:09 +0800

    RequestFactory search func done

--- AddContentToAppleMusic/AppleMusicRequestFactory.swift
+++ AddContentToAppleMusic/AppleMusicRequestFactory.swift
@@ -16,6 +16,39 @@ struct AppleMusicRequestFactory {
     
     static let userStorefrontPathURLString = "/v1/me/storefront"
  
+    //送出 search request
+    static func createSearchRequest(with term: String, countryCode: String, developerToken: String) -> URLRequest {
+        
+        let developerToken = devToken
+        
+        //把網址組合出來的方法
+        var urlComponents = URLComponents()
+        urlComponents.scheme = "https"
+        urlComponents.host = AppleMusicRequestFactory.appleMusicAPIBaseURLString
+        urlComponents.path = "/v1/catalog/\(countryCode)/search"
+        
+        //第一行，應該是把空格換掉，第二行不知道是什麼意思，但 limit 應該是一回傳的筆數固定十筆
+        let expectedTerms = term.replacingOccurrences(of: " ", with: "+")
+        let urlParameters = ["term": expectedTerms,
+                             "limit": "10",
+                             "types": "songs,albums"]
+        
+        var queryItems = [URLQueryItem]()
+        for (key, value) in urlParameters {
+            queryItems.append(URLQueryItem(name: key, value: value))
+        }
+        
+        urlComponents.queryItems = queryItems
+        
+        var urlRequest = URLRequest(url: urlComponents.url!)
+        urlRequest.httpMethod = "GET"
+        
+        urlRequest.addValue("Bearer \(developerToken)", forHTTPHeaderField: "Authorization")
+        
+        return urlRequest
+        
+    }
+    
     
     
 }
