bb08a2e30c7f39f7fdf81aea1e1825e9d3ec5b5c
Marvin Lin <atimis19@gmail.com>
2017-07-28 09:08:38 +0800

    Pods installed

---
+++ AddContentToAppleMusic/TestiTuneSearchViewController.swift
@@ -0,0 +1,42 @@
+//
+//  TestiTuneSearchViewController.swift
+//  AddContentToAppleMusic
+//
+//  Created by moon on 2017/7/28.
+//  Copyright © 2017年 Marvin Lin. All rights reserved.
+//
+
+import UIKit
+
+class TestiTuneSearchViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func startButtonTapped(_ sender: UIButton) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Pods/Target Support Files/Pods-AddContentToAppleMusic/Pods-AddContentToAppleMusic-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AddContentToAppleMusic : NSObject
+@end
+@implementation PodsDummy_Pods_AddContentToAppleMusic
+@end
